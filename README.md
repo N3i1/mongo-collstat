dbfff9259c78870fa4a010987f6ff73be564801f
Neil C <neilc@dba-bots.com>
2018-01-13 19:56:17 +0000

    code update

--- README.md
+++ README.md
@@ -1,13 +1,65 @@
- DBA reporting tool for mongodb
-================================
+Reporting tool for mongoDB
+===========================
+dba-mongo-report
 
-Creates a report for all non system db's by default. The metrics are gathered from collStats() and indexstats()
+Uses metrics from collStats and indexstats at present and generate a simple report.
 
-### SCREENSHOTS
+### Usage
 
+ You can run dba-mongo-report for either;
+ 
+  * The mongo instance as a whole
+  ```python
+    dba-mongo-report.py 
+```
+  * A single db, along with all its collections
+  ```python
+    dba-mongo-report.py -d test 
+```
+  * A specific collection within a db
+  ```python
+    dba-mongo-report.py -d test -c restaurants
+```
+   * For help
+   ```python
+   dba-mongodb-reporting.py -h 
+   
+usage: dba-mongo-report.py [-h] [-db DB_NAME] [-coll COLL_NAME]
+
+dba-mongodb-reporting
+
+optional arguments:
+  -h, --help       show this help message and exit
+  -db DB_NAME      db name
+  -coll COLL_NAME  collection name
+```
+    
+The report covers:
+  
+  * Overview of the collection/s within the db/s
+  * Wiredtiger stats for that collection
+  * Index stats for all the indexes for that collection/s
+
+### Required packages
+
+pip install pymongo
+pip install argparse
+  
+### Requirments
+
+ Mongodb running wired tiger storage engine
+  
+
+### Sample
+
+Running:
+
+ ```python
+    main.py -d test -c restaurants
+```
+Would yeild:
 
 ![alt tag](screenshots/db_report.PNG)
 
-![alt tag](screenshots/inx_report.PNG)
 
 
--- mongo.py
+++ mongo.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
+import pymongo
+"""
+ 
+"""
+
 
 class Mongodb(object):
-
     HOST = "localhost"
     PORT = int(27017)
 
-    def __init__(self, host=None, port=None, username=None, password = None):
+    def __init__(self, host=None, port=None, username=None, password=None, args_results=None):
 
+        self.args_results = args_results
         if host is None:
             self.host = self.HOST
         else:
@@ -22,25 +27,39 @@ class Mongodb(object):
         if password is None:
             self.password = None
 
+        try:
+            self.mongo_conn = Mongodb.est_mongodb_connection(self)
 
-    def get_db_names(self, mongodb_conn, list_all="no"):
+        except ConnectionError as e:
+            print(e)
+
+    def get_db_names(self, arg_results, list_all="no"):
         """
 
         :param mongodb_conn:
         :return: list of database names
         """
+        try:
+            db_names = list(self.mongo_conn.database_names())
+        except EnvironmentError:
+            print("no db names")
+
+        if arg_results.db_name is not None:
+            try:
+                if arg_results.db_name in db_names:
+                    db_names.clear()
+                    db_names.append(arg_results.db_name)
+            except ValueError:
+                print("db not exist")
+
         if list_all is "no":
-            db_names = list(mongodb_conn.database_names())
             if "admin" in db_names:
                 db_names.remove("admin")
             if "config" in db_names:
                 db_names.remove("config")
             if "local" in db_names:
                 db_names.remove("local")
-        else:
-            db_names = list(mongodb_conn.database_names())
-        return list(db_names)
-
+        return db_names
 
     def get_index_names(self, parent_stat):
         """
@@ -51,8 +70,8 @@ class Mongodb(object):
         # TODO splice first entry which is the index name for the given collection
         return list(parent_stat)
 
+    def get_collection_names(self, arg_results, mongodb_db_names):
 
-    def get_collection_names(self, mongodb_conn, mongodb_db_names):
         """
 
         :param mongodb_conn:
@@ -60,13 +79,26 @@ class Mongodb(object):
         :return:
         """
         coll_names_dict = {}
+        coll = []
         for i in mongodb_db_names:
-            coll = []
-            for c in mongodb_conn[i].collection_names():
-                coll.append(c)
+            coll = self.mongo_conn[i].collection_names()
             coll_names_dict[i] = coll
-        return coll_names_dict
 
+        # When we get to here we have a dict(list[])
+        if arg_results.coll_name is not None:
+            for key in coll_names_dict.keys():
+                if arg_results.coll_name not in coll_names_dict[key]:
+                    print("not found")
+                    exit()
+                else:
+                    if arg_results.coll_name in coll_names_dict[key]:
+                        # the collecton is in this key -
+                        coll_names_dict = {}
+                        coll.clear()
+                        coll.append(arg_results.coll_name)
+                        coll_names_dict[key] = coll
+                    #return coll_names_dict
+        return coll_names_dict
 
     def est_mongodb_connection(self):
         if self.username and self.password == None:
@@ -74,3 +106,128 @@ class Mongodb(object):
         else:
             mongo_conn = MongoClient(host=self.host, port=self.port, username=self.username, password=self.password)
         return mongo_conn
+
+    def print_indexstats_results(self, index_stat_results):
+        pass
+
+    def print_collstat_results(self, mongodb_collstats_metrics):
+        # TODO Add in a flag to speficy DB name
+        Child = ["LSM", "block-manager", "btree", "cache", "compression", "cursor", "reconciliation", "session",
+                 "transaction"]
+        for db_name in mongodb_collstats_metrics:
+            print(db_name)
+            for collection_name in mongodb_collstats_metrics[db_name]:
+                print("\t", collection_name)
+                coll_stats_parent_doc = data.parse_collstat_parent_doc(
+                    mongodb_collstats_metrics[db_name][collection_name],
+                    "wiredTiger")
+                for v in Child:
+                    print("\t##", v, ":")
+                    coll_stats_child_doc = data.parse_collstat_sub_docs(coll_stats_parent_doc, v)
+                    for key, value in coll_stats_child_doc.items():
+                        if value != 0:
+                            print("\t\t", key, ":", value)
+
+    def run_indexstats(self, db_name, collection_name):
+        """
+
+        :param mongodb_conn:
+        :param db_name:
+        :param collection_name:
+        :return: {index_name : index_stats_doc }
+        """
+        # TODO Either specify a single db and/or collection, or by default it will call for each collection in a db
+        pipeline = [{"$indexStats": {}}]
+        index_stat_results = {}
+        query = pymongo.collection.Collection(self.mongo_conn[db_name], name=collection_name)
+        cursor = query.aggregate(pipeline)
+        res = list(cursor)
+        for v in res:
+            name = v['name']
+            index_stat_results[name] = v
+        return index_stat_results
+
+    def run_collstats(self, mongo_db_names, mongodb_coll_names):
+        """
+        Return a dict() =  { collection_name: collection_stats{} }
+        """
+        coll_stats_results = {}
+        for db_name in mongo_db_names:
+            for collection_name in mongodb_coll_names[db_name]:
+                coll_stats = self.mongo_conn[db_name].command("collstats", collection_name)
+                coll_stats_results[collection_name] = coll_stats
+        return coll_stats_results
+
+    def run_dbServerStatus(self):
+        stat = self.mongo_conn["admin"].command("serverStatus")
+        return stat
+
+    def run_report(self, mongo_db_names, mongo_coll_names):
+        # TODO Add options so you can pick which statst to run
+        # TODO Add flags so you can specify just one db and/or collection
+        """Child = ["LSM", "block-manager", "btree", "cache", "compression", "cursor", "reconciliation", "session",
+                 "transaction"]"""
+        coll_stats_results = Mongodb.run_collstats(self, mongo_db_names, mongo_coll_names)
+        for db_name in mongo_coll_names:
+
+            print("!-------------------- MONGODB Collection STATS --------------------!")
+            print("\n\t\tDATABASE NAME:", db_name)
+            for collection_name in mongo_coll_names[db_name]:
+                if collection_name in coll_stats_results:
+                    print("\n\tCollection Name:", collection_name)
+                    print("\tTotal size in memory:", coll_stats_results[collection_name]['size'])
+                    print("\tTotal amount of storage allocated:", coll_stats_results[collection_name]['storageSize'])
+                    print("\tTotal number of documents:", coll_stats_results[collection_name]['count'])
+                    # print("\tAvg size of object in collection:", coll_stats['avgObjSize'])
+                    print("\tTotal amount of storageallocated for documents",
+                          coll_stats_results[collection_name]['storageSize'])
+                    print("\tTotal amount of storage used by Indexes:",
+                          coll_stats_results[collection_name]['totalIndexSize'])
+
+                    print("\n\t\t\tWired Tiger Storage Stat:\n")
+                    coll_stats_parent_doc = Mongodb.parse_collstat_parent_doc(self, coll_stats_results[collection_name],
+                                                                              "wiredTiger")
+                    Child = ["btree", "cache", "cursor", "session"]
+                    for v in Child:
+                        print("\t", v, ":")
+                        coll_stats_child_doc = Mongodb.parse_collstat_sub_docs(self, coll_stats_parent_doc, v)
+                        for key, value in coll_stats_child_doc.items():
+                            if value != 0:
+                                print("\t\t", key, ":", value)
+
+                    print("\t\t\t\n\nIndex statistics for collection:", collection_name)
+
+                    coll_stats_parent_doc = Mongodb.parse_collstat_parent_doc(self, coll_stats_results[collection_name],
+                                                                              "indexDetails")
+
+                    for key, value in coll_stats_parent_doc.items():
+                        indx_name = key
+                        print("\t\t\nINDEX NAME:", key, "\n")
+
+                        coll_stats_index_doc = coll_stats_parent_doc[key]
+                        Child = ["btree", "cache"]
+                        for v in Child:
+                            print("\t", v, ":")
+                            coll_stats_child_doc = Mongodb.parse_collstat_sub_docs(self, coll_stats_index_doc, v)
+                            for key, value in coll_stats_child_doc.items():
+                                if value != 0:
+                                    print("\t\t\t", key, ":", value)
+                        print("\t\t\nIndex usage:", "\n")
+                        # Call indexStats on a per db basis
+                        # TODO move this call out of this module
+                        index_stat_results = Mongodb.run_indexstats(self, db_name, collection_name)
+                        for key, value in index_stat_results.items():
+                            if key == indx_name:
+                                print("\tNo of Ops:", value['accesses']['ops'])
+
+    def parse_collstat_parent_doc(self, collstat_stats, parent_stat_name):
+        return dict(collstat_stats[parent_stat_name])
+
+    def parse_collstat_sub_docs(self, stat_name, child_stat_name):
+        return dict(stat_name[child_stat_name])
+
+    def parse_index_doc(self, parent_stat, index_name):
+        # TODO Handle a list of indexes
+        return dict(parent_stat[index_name])
+
+# if __name__ == "__main__":
